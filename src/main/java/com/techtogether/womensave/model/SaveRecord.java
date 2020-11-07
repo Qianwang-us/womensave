3ae537725998d27e1c2e86e0401897b37bfe002c
Qianwang-us <qianwang.us@gmail.com>
2020-11-07 11:17:52 -0500

    update models

--- src/main/java/com/techtogether/womensave/model/SaveRecord.java
+++ src/main/java/com/techtogether/womensave/model/SaveRecord.java
@@ -1,5 +1,31 @@
 package com.techtogether.womensave.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="save_records")
 public class SaveRecord {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer id;
+	
+	@Column(name="year")
+	private int year;
+	
+	@Column(name="month")
+	private int month;
+	
+	@Column(name="month_goal")
+	private double monthGoal;
+	
+	@Column(name="month_saved_amount")
+	private double monthSavedAmount;
+	
 }
--- src/main/java/com/techtogether/womensave/model/User.java
+++ src/main/java/com/techtogether/womensave/model/User.java
@@ -1,7 +1,14 @@
 package com.techtogether.womensave.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -9,9 +16,21 @@ import javax.persistence.Table;
 public class User {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private Integer id;
+	
+	@Column(name="name", nullable=false)
 	private String name;
+	
+	@Column(name="email", unique=true, nullable=false)
 	private String email;
+	
+	@Column(name="password", nullable=false)
 	private String password;
+	
+	@OneToMany(mappedBy="user", cascade = CascadeType.ALL)
+	private List<Goal> yearGoals;
+	
 
 }
