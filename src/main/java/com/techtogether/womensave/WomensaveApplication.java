d81d3e2e485506c232cc19365bfcb1c7883dad19
Qianwang-us <qianwang.us@gmail.com>
2020-11-07 08:01:30 -0500

    initial commit

---
+++ src/main/java/com/techtogether/womensave/WomensaveApplication.java
@@ -0,0 +1,13 @@
+package com.techtogether.womensave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomensaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WomensaveApplication.class, args);
+	}
+
+}
